fd3473b7ea82db6c29e72268eed4f62b03ed8543
coolcjg <coolcjg@naver.com>
2024-01-03 23:09:02 +0900

    대표 미디어 영역 작업중

--- pages/board/[boardId]..js
+++ pages/board/[boardId]..js
@@ -86,7 +86,7 @@ const Index = ({data}) => {
                                                 <path d="M9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.5zm0 1v2A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z"/>
                                                 </svg>
                                         }
-                                        <img className="mediaThumbImg" src={media.thumbImgUrl}/>
+                                        <img className="mediaThumbImg" src={media.thumbnailImgUrl} onClick={()=> setMainMedia(media)}/>
                                     </div>
                                 ))
                             }
