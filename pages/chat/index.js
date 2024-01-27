6a1461e1272cfae494af99bacec66b954f269556
coolcjg <coolcjg@naver.com>
2024-01-27 16:25:48 +0900

    채팅 퍼블 작성중

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -1,6 +1,20 @@
 import Header from "../components/header";
+import {useState} from 'react';
 
 const Index = ({}) => {
+
+    const [userId, setUserId] = useState('coolcjg');
+    console.log("여기먼저? : " + userId);
+    const [chatList, setChatList] = useState([
+        {type:'enter', userId:'coolcjg', message:'', time:'15:00'},
+        {type:'enter', userId:'testUser', message:'', time:'15:01'},
+        {type:'enter', userId:'testUser2', message:'', time:'15:02'},
+        {type:'exit', userId:'testUser2', message:'', time:'15:03'},
+        {type:'message', userId:'coolcjg', message:'아무말이나 하자11', time:'16:00'},
+        {type:'message', userId:'testUser', message:'아무말이나 하자22', time:'16:01'},
+        {type:'message', userId:'testUser', message:'아무말이나 하자33', time:'16:02'},
+        {type:'quit', userId:'', message:'', time:'16:02'},
+    ]);
 
     return (
         <>
@@ -12,16 +26,78 @@ const Index = ({}) => {
                     </div>
 
                     <div className="chatBody">
-                        <div>
+                        {
+                            chatList.length == 0 &&
+                            <div className="chat-notice">
+                                채팅 내용이 없습니다.
+                            </div>                           
+                        }
 
-                        </div>
+                        {
+                            chatList.length > 0 && chatList.map((chat, index) =>{
+                                if(chat.type === 'enter'){
+                                    return(
+                                        <div className="chat-notice" key={index}>
+                                            {chat.userId + "님이 접속하였습니다"}
+                                        </div>                                   
+                                    )
+                                }else if(chat.type === 'exit'){
+                                    return(
+                                            <div className="chat-notice" key={index}>
+                                                {chat.userId + "님이 퇴장하였습니다."}
+                                            </div>                                   
+                                        )
+                                }else if(chat.type === 'message' ){
+                                    if(chat.userId === userId){
+                                        return(
+                                            <div className="chat-me" key={index}>
+                                                <div className="chat-message-user"><span>{chat.time}</span>{chat.userId}</div>
+                                                <div>
+                                                {chat.message}
+                                                </div>
+                                            </div>
+                                        )
+                                    }else{
+                                        return(
+                                            <div className="chat-other" key={index}>
+                                                <div className="chat-message-user">{chat.userId}<span>{chat.time}</span></div>
+                                                <div>
+                                                {chat.message}
+                                                </div>
+                                            </div>                                            
+                                        )
+
+                                    }
+                                }else if(chat.type ==='quit'){
+                                    return(
+                                        <div className="chat-notice" key={index}>
+                                            채팅방이 종료되었습니다.
+                                        </div>                                          
+                                    )
+                                }
+
+                            })
+                        }
+
+                      
+                        
                     </div>
 
                     <div className="chatInput">
-                        <div className="messageDiv" contenteditable="true">
+                        <div className="messageDiv" contentEditable="true">
                         </div>
                         <div className="sendDiv">
-                            <button>입력</button>
+                            <svg width="3.9rem" height="3.9rem" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clipPath="url(#clip0_15_829)">
+                            <rect width="24" height="24" fill="white"/>
+                            <path d="M19.364 5.05026L3.10051 8.58579L10.8787 13.5355M19.364 5.05026L15.8284 21.3137L10.8787 13.5355M19.364 5.05026L10.8787 13.5355" stroke="#000000" strokeLinecap="round" strokeLinejoin="round"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_15_829">
+                            <rect width="24" height="24" fill="white"/>
+                            </clipPath>
+                            </defs>
+                            </svg>                        
                         </div>
                     </div>
 
