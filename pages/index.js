af7add7a3281ab79218d7b497b57f260da828e4b
coolcjg <coolcjg@naver.com>
2024-06-19 20:08:39 +0900

    7799

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Index = ({data}) => {
     return (
         <>
             <Header></Header>
-            <div className="center">HELLO WORLD7788</div>
+            <div className="center">HELLO WORLD7799</div>
         </>
     )
 };
