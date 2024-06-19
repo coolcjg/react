a7ad7d7b84af5b6b0eb998579042ab315e10a926
coolcjg <coolcjg@naver.com>
2024-06-19 18:15:12 +0900

    쉘 스크립트 변경

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,1 +1,16 @@
-echo "> after install"
+#!/bin/bash
+var=$(ps -ef |grep next-server |grep -v 'grep'|grep -v $0)
+echo ${var}
+pid=$(echo ${var} | cut -d " " -f2)
+
+if [ -n "${pid}" ]
+then
+  kill -9 ${pid}
+  echo $* is terminated.
+
+else
+  echo $* is not running.
+fi
+
+cd /home/ec2-user/test
+npm run start &
